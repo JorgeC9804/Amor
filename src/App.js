4ee918253d790a3a76ac3b00ff78adc248135d66
Jorge <jorgs705@gmail.com>
2023-05-06 19:42:32 -0600

    update 1.0.0

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import amor from "./img/img-test/amairani.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="text-jorge">hellow world</h1>
+        <h2 className="text-amairani">hellow world</h2>
+        <h3>hellow world</h3>
+        <h4>hellow world</h4>
+        <img src={amor} alt="amairani" />
       </header>
     </div>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      // text-
+      jorge: "#1fb6ff",
+      amairani: "#FDE600",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
